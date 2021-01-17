dc76fc0ac3699aee4911656464e17d70788f7f25
Laura <laurabezaq@gmail.com>
2021-01-17 18:25:05 +0100

    creado test modelo

--- src/app/models/voting.model.ts
+++ src/app/models/voting.model.ts
@@ -1,23 +1,25 @@
 import {jsonArrayMember, jsonMember, jsonObject} from 'typedjson';
 
 @jsonObject
-class Auth {
+export class Auth {
   @jsonMember({constructor: String})
   name: string;
   @jsonMember({constructor: String})
   url: string;
   @jsonMember({constructor: Boolean})
   me: boolean;
+  constructor(name:string,url:string,me:boolean){}
 }
 
 @jsonObject
-class PubKey {
+export class PubKey {
   @jsonMember({constructor: Number})
   p: number;
   @jsonMember({constructor: Number})
   g: number;
   @jsonMember({constructor: Number})
   y: number;
+  constructor(p:number, g:number, y:number){}
 }
 
 @jsonObject
@@ -26,6 +28,7 @@ export class Option {
   number: number;
   @jsonMember({constructor: String})
   option: string;
+  constructor(number:number, option:string){}
 }
 
 @jsonObject
@@ -34,6 +37,7 @@ export class Question {
   desc: string;
   @jsonArrayMember(Option)
   options: Option[];
+  constructor(desc: string, options: Option[]){}
 }
 
 @jsonObject
@@ -61,4 +65,6 @@ export class Voting {
   tally: null;
   @jsonMember({constructor: Object})
   postproc: null;
+  constructor(id:number, name:string, desc:string, question:Question, start_date: Date, end_date: Date,
+    pub_key: PubKey,auths: Auth[],tally: null,postproc: null){}
 }
